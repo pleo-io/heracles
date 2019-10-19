f25f0116b73277715c77314b7538cf69aa2ef09c
alexromanov <al8x.romanov@gmail.com>
2019-10-19 18:55:18 +0300

    Add Spring Boot generated starter project

---
+++ src/main/java/com/alexromanov/heracles/HeraclesApplication.java
@@ -0,0 +1,13 @@
+package com.alexromanov.heracles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeraclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeraclesApplication.class, args);
+	}
+
+}
