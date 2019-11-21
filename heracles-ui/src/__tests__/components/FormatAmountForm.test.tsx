a6fd76e8ab48fdeb2854c97ceb5cdfa9734c66c3
Kelvin Wahome <kevowahome@gmail.com>
2019-11-21 22:23:36 +0300

    Tests
    Dockerization
    Use circleci/openjdk:11.0.3-jdk-stretch image in circleci

--- heracles-ui/src/__tests__/components/FormatAmountForm.test.tsx
+++ heracles-ui/src/__tests__/components/FormatAmountForm.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormatAmountForm from '../../components/FormatAmountForm'
 
 describe("<FormatAmountForm />", () => {
-    test("return the correct formatted amount", () => {
+    test("should return the correct formatted amount", () => {
 
     });
 });
