4470182b024ec5ed8d4ff604f5ce7d6c02ff3872
Lucas de Lima <lucas.de.lima@engineer.com>
2019-12-07 10:00:28 -0300

    Performance tests: Added containers for performance tests

--- load-tests/tests/performance-test.js
+++ load-tests/tests/performance-test.js
@@ -1,25 +1,23 @@
-import { group, sleep, check, fail } from 'k6';
+import {group, sleep, check} from 'k6';
 import http from 'k6/http';
-import { Rate } from 'k6/metrics';
+import {Rate} from 'k6/metrics'
+import {exportOptions} from "./options/rampUpTestOption.js";
 
+export let options = exportOptions;
 
-
-export let options = {
-    vus: 1,
-    duration: "1m"
-}
+export let incorrectStatusCode = new Rate("incorrect status code");
 
 
 export default function() {
-    let req, res, headers;
-    group('Marketing page requests', () => {
-        
-        let url = 'https://www.google.com';
 
-        res = http.get(url, params);
+    group("Load test file", function() {
+        let req, res;
+        let url = 'https://www.google.com';
+        res = http.get(req);
+
         check(res, {
-            "status code is 200": (res) => res.status === 200,
-        });
-        sleep(1);
+            "status code is correct": r => r.status === (200)
+        }) || incorrectStatusCode.add(1);
+        sleep(3);
     });
 }
